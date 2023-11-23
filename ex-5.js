980dea807ede42782dedafb3669773fc1793194a
Parunyu Pitragool <Mek@Meks-MacBook-Pro.local>
2023-11-23 14:57:32 +0700

    feat: add complete exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalProductPrice = 27000;
+let productDiscount = totalProductPrice * (25 / 100);
+console.log(productDiscount); // 6750
+let netProductPrice = totalProductPrice - productDiscount;
+console.log(netProductPrice); // 20250
