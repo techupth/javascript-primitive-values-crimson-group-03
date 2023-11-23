f4b463520f75965c3352beb860a9f7d0c0760c3a
nattawat <frank.nattawat@hotmail.com>
2023-11-23 15:10:03 +0700

    feat:complete ex primitive

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
-
+let totalPrice = 27000
+const discount = 25/100
+let finalyPrice = totalPrice-(totalPrice*discount) +" Bath";
+console.log(finalyPrice);
 // Start coding here
